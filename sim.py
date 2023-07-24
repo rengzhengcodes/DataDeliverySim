e9bc9e072dafcdb615a7f0b880d6721f5944626e
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-07-24 12:01:37 -0400

    some random key_error

--- sim.py
+++ sim.py
@@ -8,6 +8,8 @@ from elements import Feature
 from elements.buffer import Buffer
 from elements.processing import PE
 
+from topology import Topology
+
 # Length and width of the grid representing the topology.
 M: int = 100
 N: int = 100
@@ -21,10 +23,10 @@ packets = list(range(num_packets))
 # List of packets that need to be placed into an src.
 src_packets = list(packets)
 
-# Set of all PEs that need a key.
-dest = {key: set() for key in packets}
-# Sources of packets.
-srcs = {key: set() for key in packets}
+# Set of all PEs.
+pes: set[PE] = set()
+# Set of all buffers.
+bufs: set[Buffer] = set()
 
 # Populates the topology, alternating rows of PEs and Buffers.
 x: int
@@ -34,106 +36,21 @@ for x in range(M):
         if x % 2 == 0:
             # Chooses 3 packets that need to be delivered.
             deliveries: set[int] = set(random.sample(packets, 3))
-            # Initializes a PE with those packets.
-            pe: PE = PE((x, y), deliveries)
-            # For each PE that needs to receive a delivery, adds the PE to the
-            # set of dests.
-            packet: int
-            for packet in deliveries:
-                dest[packet].add(pe)
+            # Instantiates a PE with those packets.
+            pes.add(PE((x, y), deliveries))
         else:
             # Chooses 1 packet from packets.
             packet: set[int] = random.choice(src_packets)
             # Removes  that packet from future packets to be put in.
             src_packets.remove(packet)
-            # Initializes a buffer with that packet.
-            buffer: Buffer = Buffer((x, y), {packet})
-            # Adds the buffer to the set of sources for that packet.
-            srcs[packet].add(buffer)
+            # Instantiates a buffer with that packet.
+            bufs.add(Buffer((x, y), {packet}))
 
-
-def diffuse_packet(pkt: int) -> tuple:
-    """
-    Diffuses a packet through the grid until it reaches all its destinations.
-
-    @pkt        The packet to diffuse.
-    @pkt_grid   A grid representing the diffusion of a packet.
-
-    @return     A tuple where the first element is the number of steps needed to
-                diffuse the packet to all elements and the second element is the
-                total number of steps taken by the diffusion.
-    """
-    # Notes the target locations.
-    target_locs: set[tuple] = {pe.loc for pe in dest[pkt]}
-    # Reference check for correct calcs.
-    ref_locs: set[tuple] = target_locs.copy()
-
-    # Initializes the diffusion grid, tracking steps from the nearest packet.
-    pkt_grid: list[list[bool]] = [[-1 for j in range(M)] for i in range(N)]
-
-    # Initializes the diffusion grid with the sources.
-    src: tuple[int, int]
-    for src in srcs[pkt]:
-        pkt_grid[src.loc[0]][src.loc[1]] = 0
-
-    # Tracks the maximum number of steps taken to reach all destinations.
-    max_steps: int = 0
-    # Tracks the total number of steps taken to reach all destinations.
-    tot_steps: int = 0
-    # lambda function to detect if a packet has reached a locations.
-    reached: callable([tuple[int, int]], bool) = (
-        lambda loc: pkt_grid[loc[0]][loc[1]] >= 0
-    )
-    # Adjacencies in the topology grid, representing the diffusion.
-    adjacencies: tuple[tuple[int, int]] = ((0, 1), (0, -1), (1, 0), (-1, 0))
-    # Runs the diffusion until all destinations are reached.
-    while any(not reached(loc) for loc in target_locs):
-        # Goes through the grid, diffusing the packet.
-        i: int
-        for i in range(M):
-            j: int
-            for j in range(N):
-                # If the packet is at this location, diffuses it.
-                if reached((i, j)):
-                    # Goes through each adjacency.
-                    adj: tuple
-                    for adj in adjacencies:
-                        # Calculates the adjacent location.
-                        adj_loc: tuple[int, int] = (i + adj[0], j + adj[1])
-                        # If the adjacent location is in the grid and has a higher
-                        # or negative step count, diffuses the packet.
-                        if (0 <= adj_loc[0] < M and 0 <= adj_loc[1] < N) and (
-                            pkt_grid[adj_loc[0]][adj_loc[1]] < 0
-                            or pkt_grid[adj_loc[0]][adj_loc[1]] > pkt_grid[i][j] + 1
-                        ):
-                            # If the adj_loc is a destination.
-                            if adj_loc in target_locs:
-                                # Diffuse 0.
-                                pkt_grid[adj_loc[0]][adj_loc[1]] = 0
-                                # Add number of steps to tot_steps.
-                                tot_steps += pkt_grid[i][j] + 1
-                                # Remove the destination from the set of target
-                                # locations.
-                                target_locs.remove(adj_loc)
-                            # Otherwise, diffuse the packet.
-                            else:
-                                pkt_grid[adj_loc[0]][adj_loc[1]] = pkt_grid[i][j] + 1
-
-        max_steps += 1
-
-    # Sanity check the program works correctly.
-    assert max_steps <= tot_steps
-    # The only case where it should be zero in a topology where buffers and
-    # PEs are discrete units is when no PEs request the data.
-    if max_steps == 0:
-        assert not ref_locs
-
-    return (max_steps, tot_steps)
-
+topology: Topology = Topology((M, N), pes, bufs)
 
 # Runs the simulation until all packets are delivered.
 for packet in packets:
-    max_steps, tot_steps = diffuse_packet(packet)
+    max_steps, tot_steps = topology.diffuse_packet(packet)
     print(
         f"Packet {packet} took {max_steps} steps to deliver, with all steps being {tot_steps}."
     )
--- topology.py
+++ topology.py
@@ -1,6 +1,9 @@
 """Abstracts out the topology of the network."""
 from __future__ import annotations
+from copy import deepcopy
+from typing import Any, Iterable
 
+from elements import Feature
 from elements.buffer import Buffer
 from elements.processing import PE
 
@@ -11,13 +14,165 @@ class Topology():
         """
         Initializes the topology class.
 
-        @dims   A tuple representing the dims of a topology grid.
+        @dims   A tuple representing the dims of a topology grid. First element
+                corresponds to the first index used to access. E.g. (x, y, z)
+                corresponds to topology[x][y][z]. 0-indexed.
         @pes    The PEs of a topology.
         @buffer The buffers of a topology.
         """
+        # Keeps track of all the input variables.
+        self._dims: tuple = dims
+        self._pes: tuple[PE] = pes
+        self._buffer: tuple[Buffer] = buffers
 
-        self._dims = dims
-        self._pes = pes
-        self._buffer = buffers
+        # Destination and source directories.
+        self._dsts: dict[Any, set] = Topology.build_directory(self._pes)
+        self._srcs: dict[Any, set] = Topology.build_directory(self._buffer)
+
+    def build_directory(features: Iterable[Feature]) -> dict[Any, set]:
+        """
+        Builds a directory corresponding the "data" property of a Feature to
+        a reference to the Feature.
+
+        @param elem     The features for which we are building the directory.
+        
+        @return         A dictionary corresponding the data identifier to a set
+                        of all features with that identifier.
+        """
+        # Output dictionary
+        ret: dict[Any, set] = {}
+
+        # Builds the destination directory for all packet ids.
+        feature: Feature
+        for feature in features:
+            # Pulls out all pkt_ids and adds the PE to the set.
+            pkt_id: Any
+            for pkt_id in feature.data:
+                # If a set doesn't exist at a pkt_id, create one
+                if pkt_id not in ret:
+                    ret[pkt_id] = set()
+                # Adds PE to the pkt_id directory
+                ret[pkt_id].add(feature)
+        
+        return ret
+
+    def build_diffusion_grid(self, dim_idx: int) -> list:
+        """Recursively builds a grid for the topological diffusion to happen on
+        
+        @param dim_idx  The index of the dim we're currently accessing.
+        """
+        # Iterator we use for intialization.
+        iterator: Iterable = range(self._dims[dim_idx])
+
+        # Recurses if we haven't reached the base case.
+        if (dim_idx < len(self._dims) - 1):
+            lower_grid: tuple = self.build_diffusion_grid(dim_idx + 1)
+            return [deepcopy(lower_grid) for i in iterator]
+
+        # Otherwise, build the lowest layer diffusion grid.
+        return [-1 for i in iterator]
+
+    def deduce_subspace(space: Iterable, coords: tuple) -> Iterable:
+        """
+        Given a multidimensional iterable, uses the coordinates given to return
+        the subspace the last coord is contained in.
+
+        @space  The space whose subspace we're deducing.
+        @coord  The coordinates within the grid we want to reach.
+
+        @return The Iterable that contains the last subspace referred to in coord.
+        """
+        # Tracks the current subspace we are in.
+        subspace: tuple = space
+        # Goes recursively through the indices until it reaches it.
+        i: int
+        coord: int
+        for i, coord in enumerate(coords):
+            # Assigns if we're on the last coord.
+            if i == len(coords) - 1:
+                return subspace
+            # Otherwise, continue reducing subspace based on coords.
+            else:
+                subspace = subspace[coord]
+
     
-    def run_propagation(self) -> :
+    def diffuse_packet(self, pkt: int) -> tuple:
+        """
+        Diffuses a packet through the grid until it reaches all its destinations.
+
+        @pkt        The packet to diffuse.
+        @pkt_grid   A grid representing the diffusion of a packet.
+
+        @return     A tuple where the first element is the number of steps needed to
+                    diffuse the packet to all elements and the second element is the
+                    total number of steps taken by the diffusion.
+        """
+        # Notes the target locations.
+        target_locs: set[tuple] = {pe.loc for pe in self._dsts[pkt]}
+        # Reference check for correct calcs.
+        ref_locs: set[tuple] = target_locs.copy()
+
+        # Initializes the diffusion grid, tracking steps from the nearest packet.
+        pkt_grid: tuple = self.build_diffusion_grid(0)
+
+        # Initializes the diffusion grid with the sources.
+        src: tuple[int, int]
+        for src in self._srcs[pkt]:
+            # Gets the smallest subspace containing the cell we want to seed.
+            subspace: tuple = Topology.deduce_subspace(pkt_grid, src.loc)
+            # Seeds that cell.
+            subspace[src.loc[-1]] = 0
+
+        # Tracks the maximum number of steps taken to reach all destinations.
+        max_steps: int = 0
+        # Tracks the total number of steps taken to reach all destinations.
+        tot_steps: int = 0
+        # lambda function to detect if a packet has reached a locations.
+        reached: callable([tuple[int, int]], bool) = (
+            lambda loc: Topology.deduce_subspace(pkt_grid, loc)[loc[-1]] >= 0
+        )
+        # Adjacencies in the topology grid, representing the diffusion.
+        adjacencies: tuple[tuple[int, int]] = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        # Runs the diffusion until all destinations are reached.
+        while any(not reached(loc) for loc in target_locs):
+            # Goes through the grid, diffusing the packet.
+            i: int
+            for i in range(self._dims[0]):
+                j: int
+                for j in range(self._dims[1]):
+                    # If the packet is at this location, diffuses it.
+                    if reached((i, j)):
+                        # Goes through each adjacency.
+                        adj: tuple
+                        for adj in adjacencies:
+                            # Calculates the adjacent location.
+                            adj_loc: tuple[int, int] = (i + adj[0], j + adj[1])
+                            # If the adjacent location is in the grid and has a higher
+                            # or negative step count, diffuses the packet.
+                            if (0 <= adj_loc[0] < self._dims[0] and 0 <= adj_loc[1] < self._dims[1]) and (
+                                pkt_grid[adj_loc[0]][adj_loc[1]] < 0
+                                or pkt_grid[adj_loc[0]][adj_loc[1]] > pkt_grid[i][j] + 1
+                            ):
+                                # If the adj_loc is a destination.
+                                if adj_loc in target_locs:
+                                    # Diffuse 0.
+                                    pkt_grid[adj_loc[0]][adj_loc[1]] = 0
+                                    # Add number of steps to tot_steps.
+                                    tot_steps += pkt_grid[i][j] + 1
+                                    # Remove the destination from the set of target
+                                    # locations.
+                                    target_locs.remove(adj_loc)
+                                # Otherwise, diffuse the packet.
+                                else:
+                                    pkt_grid[adj_loc[0]][adj_loc[1]] = pkt_grid[i][j] + 1
+
+            max_steps += 1
+
+        # Sanity check the program works correctly.
+        assert max_steps <= tot_steps
+        # The only case where it should be zero in a topology where buffers and
+        # PEs are discrete units is when no PEs request the data.
+        if max_steps == 0:
+            assert not ref_locs
+
+        return (max_steps, tot_steps)
