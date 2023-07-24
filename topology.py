0431d6675cf1ae9e34504f73ca2add3558ce72d8
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-07-24 12:56:18 -0400

    still broken"

--- topology.py
+++ topology.py
@@ -9,7 +9,8 @@ from elements import Feature
 from elements.buffer import Buffer
 from elements.processing import PE
 
-class Topology():
+
+class Topology:
     """The topology we're trying to simulate."""
 
     def __init__(self, dims: tuple, pes: tuple[PE], buffers: tuple[Buffer]) -> None:
@@ -28,9 +29,9 @@ class Topology():
         self._buffer: tuple[Buffer] = buffers
 
         # Destination and source directories.
-        self._dsts: dict[Any, set] = Topology.build_directory(self._pes)
-        self._srcs: dict[Any, set] = Topology.build_directory(self._buffer)
-        
+        self._dsts: dict[Any, set] = self.build_directory(self._pes)
+        self._srcs: dict[Any, set] = self.build_directory(self._buffer)
+
         # Heatmap of worst-case transfers.
         self._heatmap: tuple = self.build_diffusion_grid(0)
 
@@ -38,6 +39,7 @@ class Topology():
         """
         Builds adjacencies based off of the dims.
         """
+
         def sub_adj_builder(dim_idx: int = 0) -> Iterable:
             """
             Builds the sub_adjacencies.
@@ -59,20 +61,20 @@ class Topology():
             # If at base case, yield the following
             else:
                 yield from ((-1,), (0,), (1,))
-        
+
         # Makes sure not to return the 0 case.
         adj: tuple
         for adj in sub_adj_builder():
             if any(adj):
                 yield adj
 
-    def build_directory(features: Iterable[Feature]) -> dict[Any, set]:
+    def build_directory(self, features: Iterable[Feature]) -> dict[Any, set]:
         """
         Builds a directory corresponding the "data" property of a Feature to
         a reference to the Feature.
 
         @param elem     The features for which we are building the directory.
-        
+
         @return         A dictionary corresponding the data identifier to a set
                         of all features with that identifier.
         """
@@ -90,19 +92,19 @@ class Topology():
                     ret[pkt_id] = set()
                 # Adds PE to the pkt_id directory
                 ret[pkt_id].add(feature)
-        
+
         return ret
 
     def build_diffusion_grid(self, init_val: Any, dim_idx: int = 0) -> list:
         """Recursively builds a grid for the topological diffusion to happen on
-        
+
         @param dim_idx  The index of the dim we're currently accessing.
         """
         # Iterator we use for intialization.
         iterator: Iterable = range(self._dims[dim_idx])
 
         # Recurses if we haven't reached the base case.
-        if (dim_idx < len(self._dims) - 1):
+        if dim_idx < len(self._dims) - 1:
             lower_grid: tuple = self.build_diffusion_grid(init_val, dim_idx + 1)
             return [deepcopy(lower_grid) for i in iterator]
 
@@ -131,10 +133,10 @@ class Topology():
             else:
                 for i in range(self._dims[-1]):
                     yield (i,)
-        
+
         return sub_location_builder()
 
-    def deduce_subspace(space: Iterable, coords: tuple) -> Iterable:
+    def deduce_subspace(self, space: Iterable, coords: tuple) -> Iterable:
         """
         Given a multidimensional iterable, uses the coordinates given to return
         the subspace the last coord is contained in.
@@ -154,15 +156,16 @@ class Topology():
             if i == len(coords) - 1:
                 return subspace
             # Otherwise, continue reducing subspace based on coords.
-            else:
-                subspace = subspace[coord]
+            subspace = subspace[coord]
+
+        return subspace
 
     def bounds_check(self, loc: tuple) -> bool:
         """Checks if a location is in bounds."""
         for index, elem in enumerate(loc):
-            if not (0 <= elem < self._dims[index]):
+            if not 0 <= elem < self._dims[index]:
                 return False
-        
+
         return True
 
     def diffuse_packet(self, pkt: int) -> tuple:
@@ -187,7 +190,7 @@ class Topology():
 
         # Initializes the diffusion grid, tracking steps from the nearest packet.
         pkt_grid: tuple = self.build_diffusion_grid(-1)
-        
+
         # Initializes the diffusion priority queue.
         queue: list = []
 
@@ -195,7 +198,7 @@ class Topology():
         src: tuple
         for src in self._srcs[pkt]:
             # Gets the smallest subspace containing the cell we want to seed.
-            subspace: tuple = Topology.deduce_subspace(pkt_grid, src.loc)
+            subspace: tuple = self.deduce_subspace(pkt_grid, src.loc)
             # Seeds that cell.
             subspace[src.loc[-1]] = 0
             # Adds to diffusion queue
@@ -207,7 +210,7 @@ class Topology():
         tot_steps: int = 0
         # lambda function to detect if a packet has reached a locations.
         reached: callable([tuple[int, int]], bool) = (
-            lambda loc: Topology.deduce_subspace(pkt_grid, loc)[loc[-1]] >= 0
+            lambda loc: self.deduce_subspace(pkt_grid, loc)[loc[-1]] >= 0
         )
         # Goes through the grid, diffusing the packet.
         while any(not reached(loc) for loc in target_locs) and queue:
@@ -219,12 +222,13 @@ class Topology():
                     # Calculates the adjacent location.
                     adj_loc: tuple = tuple(np.array(adj) + np.array(loc))
                     # Accesses the value at location.
-                    loc_val: int = Topology.deduce_subspace(pkt_grid, loc)[loc[-1]]
+                    loc_val: int = self.deduce_subspace(pkt_grid, loc)[loc[-1]]
                     # If the adjacent location is in the grid and has a higher
                     # or negative step count, diffuses the packet.
                     if (self.bounds_check(adj_loc)) and (
-                        loc_val < 0 or
-                        loc_val > Topology.deduce_subspace(pkt_grid, adj_loc)[adj_loc[-1]] + 1
+                        loc_val < 0
+                        or loc_val
+                        > self.deduce_subspace(pkt_grid, adj_loc)[adj_loc[-1]] + 1
                     ):
                         # Appends diffusion to end of queue.
                         queue.append(adj_loc)
@@ -239,10 +243,11 @@ class Topology():
                             target_locs.remove(adj_loc)
                         # Otherwise, diffuse the packet.
                         else:
-                            pkt_grid[adj_loc[0]][adj_loc[1]] = pkt_grid[loc[0]][loc[1]] + 1
+                            pkt_grid[adj_loc[0]][adj_loc[1]] = (
+                                pkt_grid[loc[0]][loc[1]] + 1
+                            )
 
         max_steps += 1
-
 
         # Sanity check the program works correctly.
         assert max_steps <= tot_steps <= (max_steps * num_locs)
